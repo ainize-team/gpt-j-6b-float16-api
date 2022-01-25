813c48df5789a5e706abc78b740162118b65e16f
YoungJae Kim <byeongal@comcom.ai>
2022-01-25 15:13:19 +0900

    add return type

--- app/api/prediction.py
+++ app/api/prediction.py
@@ -4,6 +4,7 @@ from starlette.requests import Request
 from core.config import TASK
 from core.commons import Tasks
 from models.payload import TextGenerationPredictPayload
+from models.prediction import TextGenerationResult
 
 router = APIRouter()
 
@@ -12,7 +13,7 @@ router = APIRouter()
 def post_text_generation(
         request: Request,
         block_data: TextGenerationPredictPayload
-):
+) -> TextGenerationResult:
     model = request.app.state.model
     prediction = model.predict(block_data)
     return prediction
